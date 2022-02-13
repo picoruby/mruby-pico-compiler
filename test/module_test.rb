c8dcba5833d85c35ed3ba93140d333ad65aa3950
HASUMI Hitoshi <hasumikin@gmail.com>
2022-02-14 08:26:21 +0900

    add a test of OP_MODULE https://github.com/picoruby/picoruby/issues/82

---
+++ test/module_test.rb
@@ -0,0 +1,19 @@
+class ModuleTest < PicoRubyTest
+
+  if @@vm_select == :mruby
+
+    desc "include a Module"
+    assert_equal(<<~RUBY, "0")
+      module A
+        def a
+          p 0
+        end
+      end
+      include A
+      self.a
+    RUBY
+
+  end
+
+end
+
