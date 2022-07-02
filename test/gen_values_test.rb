1ecffa7d2d1e37947f2efda7a794745ef62a373d
HASUMI Hitoshi <hasumikin@gmail.com>
2022-07-02 15:27:20 +0900

    Fix a wrong test

--- test/gen_values_test.rb
+++ test/gen_values_test.rb
@@ -3,9 +3,9 @@ class GenValuesTest < PicoRubyTest
   if @@vm_select == :mruby
 
     desc "gen_values() in generator.c"
-    assert_equal(<<~RUBY, "1\n[2, 3]\n{:a=>0}\n4\nhello")
+    assert_equal(<<~RUBY, "1\n[2, 3, 4]\n{:a=>0}\n{}\nhello")
       a = ->(m,*rest,m2,**opts,&block) do
-        p m,rest,opts,m2; block.call
+        p m,rest,m2,opts; block.call
       end
       a.call(1,2,3,4,{a: 0}) {puts :hello}
     RUBY
